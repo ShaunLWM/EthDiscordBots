7a97211cc8174f39c3629b82e484e30367ca3ad5
ShaunLWM <shaunlimweimin@gmail.com>
2021-07-25 09:13:39 +0800

    Use Binance WS for price ticket

--- src/EthPrice.ts
+++ src/EthPrice.ts
@@ -1,35 +1,34 @@
 import "dotenv-safe/config";
 
 import Discord from "discord.js";
-import fetch from "node-fetch";
-
-import { ICoinGeckoApi } from "./@types";
+import WebSocket from "ws";
 
 const client = new Discord.Client();
 let timeset = +new Date();
 
 client.once("ready", async () => {
 	console.log("[EthPrice] Ready");
-	try {
-		setInterval(async () => {
-			if (+new Date() < timeset + 5000) return;
-			const results = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd");
-			const json = (await results.json()) as ICoinGeckoApi;
-			if (!client.user || !json.ethereum) return;
-			await client.user.setPresence({
-				status: "online",
-				activity: {
-					name: `$${json.ethereum.usd}`,
-					type: "WATCHING",
-					url: "https://www.twitch.tv/somecooltwitchstreamer",
-				},
-			});
+	const ws = new WebSocket("wss://stream.binance.com:9443/stream?streams=ethusdt@ticker");
 
-			timeset = +new Date();
-		}, 2500);
-	} catch (error) {
-		console.error(error);
-	}
+	ws.on("open", () => {
+		console.log(`Opened`);
+	});
+
+	ws.on("message", async (message: string) => {
+		if (+new Date() < timeset + 5000) return;
+		if (!client.user || !message) return;
+		const json = JSON.parse(message) as BinanceWSStream;
+		await client.user.setPresence({
+			status: "online",
+			activity: {
+				name: `$${Number(json.data.c).toFixed(2)}`,
+				type: "WATCHING",
+				url: "https://www.twitch.tv/somecooltwitchstreamer",
+			},
+		});
+
+		timeset = +new Date();
+	});
 });
 
 client.login(process.env.BOT_ETHPRICE_TOKEN);
