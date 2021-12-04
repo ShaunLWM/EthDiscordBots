cfb9f6964e74aa8a764e5f440d98ab994c46c932
ShaunLWM <shaunlimweimin@gmail.com>
2021-12-04 13:15:39 +0800

    Use CoinGecko price instead

--- src/EthPrice.ts
+++ src/EthPrice.ts
@@ -1,51 +1,32 @@
 import "dotenv-safe/config";
 
 import Discord from "discord.js";
-import WebSocket from "ws";
+import fetch from "node-fetch";
 
 const client = new Discord.Client();
-let timeset = +new Date();
 
-client.once("ready", async () => {
-	console.log("[EthPrice] Ready");
-	if (!process.env.CHANNEL_LOGS) throw new Error("No CHANNEL_LOGS env");
+interface CgResult {
+	ethereum: {
+		usd: number;
+	};
+}
 
-	const logChannel = await client.channels.fetch(process.env.CHANNEL_LOGS as string);
-	if (!logChannel.isText()) throw new Error("Not a text channel");
-
-	const ws = new WebSocket("wss://stream.binance.com:9443/stream?streams=ethusdt@ticker");
-
-	ws.on("open", () => {
-		console.log(`Opened`);
-	});
-
-	ws.on("message", async (message: string) => {
-		if (+new Date() < timeset + 5000) return;
-		if (!client.user || !message) return;
-		const json = JSON.parse(message) as BinanceWSStream;
-		await client.user.setPresence({
-			status: "online",
-			activity: {
-				name: `$${Number(json.data.c).toFixed(2)}`,
-				type: "WATCHING",
-				url: "https://www.twitch.tv/somecooltwitchstreamer",
-			},
-		});
-
-		timeset = +new Date();
-	});
-
-	ws.on("ping", () => {
-		ws.pong();
-	});
-
-	ws.on("error", async (e) => {
-		await logChannel.send(JSON.stringify(e.message));
-	});
-
-	ws.on("close", async () => {
-		await logChannel.send(`Disconnected.. awaiting new connection..`);
-	});
+client.login(process.env.BOT_ETHPRICE_TOKEN).then(async () => {
+	setInterval(async () => {
+		if (!client.user) return;
+		try {
+			const result = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd");
+			const json = (await result.json()) as CgResult;
+			await client.user.setPresence({
+				status: "online",
+				activity: {
+					name: `$${Number(json.ethereum.usd).toFixed(2)}`,
+					type: "WATCHING",
+					url: "https://www.twitch.tv/somecooltwitchstreamer",
+				},
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}, 5000);
 });
-
-client.login(process.env.BOT_ETHPRICE_TOKEN);
